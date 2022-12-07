5d05ec425c13c7322a091373e74bf13af4d7291f
clixe12 <elias28598@gmail.com>
2022-12-07 09:12:54 +0100

    Add files via upload

--- Script1.js
+++ Script1.js
@@ -5,15 +5,35 @@ window.onload = ()=>
 }
 function cargarModelos()
 {
-
-	let latitud=37.19222616773243
-	let longitud=-3.616590107857667
-
+	let modelos=[
+	{
+		latitud:37.19222616773243
+		longitud:-3.616590107857667
+		color:"blue"
+		scala: 0.2
+	}
+	{
+		latitud:37.19222616773243
+		longitud:-3.616590107857667
+		color:"yellow"
+	}
+	{
+		latitud:37.19222616773243
+		longitud:-3.616590107857667
+		color:"red"
+	}
+	];
+	//let latitud=37.19222616773243
+	//let longitud=-3.616590107857667
+	for(let i =0;i>modelos.length;i++)
+	{
 	var modelo= document.createElement('a-box')
-	modelo.setAttribute('material','color:yellow')
-	modelo.setAttribute('scale''0.5 0.5 0.5')
-	model.setAttribute('gps-entity-place', `latitude: ${latitud}; longitude: ${longitud};`);
+	modelo.setAttribute('material','color:' + modelos[i].color)
+	modelo.setAttribute('scale',""+modelos[i].escala" "+modelos[i].escala" "+modelos[i].escala)
+	model.setAttribute('gps-entity-place', `latitude: ${modelos[i].latitud}; longitude: ${modelos[i].latitud};`);
 	
 	let scene = document.querySelecto('a-scene');
 	scene.appendChild(modelo);
+	}
+
 }
