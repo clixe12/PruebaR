9b92b8eb738feca1c9df8cdb89e463cc321752ed
clixe12 <elias28598@gmail.com>
2022-12-08 15:58:35 +0100

    Update and rename ScriptCasa.js to Script1.js

---
+++ Script1.js
@@ -0,0 +1,35 @@
+// JavaScript source code
+window.onload = ()=>
+{
+	cargarModelos()
+}
+function cargarModelos()
+{
+	let modelos=[
+	{ //Tuto
+		latitud:37.19226594089006
+		longitud:-3.6166366592057346
+		color:"blue"
+		scala: 0.2
+	}
+	{ //casa
+		latitud:36.99205134776458
+		longitud:-3.568055075527317
+		color:"yellow"
+	}
+
+	];
+	//let latitud=37.19222616773243
+	//let longitud=-3.616590107857667
+	for(let i =0;i>modelos.length;i++)
+	{
+	var modelo= document.createElement('a-box')
+	modelo.setAttribute('material','color:' + modelos[i].color)
+	modelo.setAttribute('scale',""+modelos[i].escala" "+modelos[i].escala" "+modelos[i].escala)
+	model.setAttribute('gps-entity-place', 'latitude: ${modelos[i].latitud}; longitude: ${modelos[i].latitud};');
+	
+	let scene = document.querySelecto('a-scene');
+	scene.appendChild(modelo);
+	}
+
+}
