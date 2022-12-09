a8c0d26d8f989b94f936c711cb021f770c959dce
clixe12 <elias28598@gmail.com>
2022-12-09 08:34:43 +0100

    Update Script1.js

--- Script1.js
+++ Script1.js
@@ -31,7 +31,7 @@ function cargarModelos()
 	model.setAttribute('gps-entity-place', `latitude: ${modelos[i].latitud}; longitude: ${modelos[i].latitud};`);
 		
 	
-	let scene = document.querySelect('a-scene');
+	let scene = document.querySelector('a-scene');
 	scene.appendChild(model);
 	}
 
